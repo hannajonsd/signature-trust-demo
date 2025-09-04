8d4528b31374541b7b8921bc72b11387cdd7e557
Hanna Margrét Jónsdóttir <hanna.jonsdottir24@imperial.ac.uk>
2025-09-04 20:57:27 +0100

    Testing new key

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gorilla/mux"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("OK"))
-    })
-    http.ListenAndServe(":8080", r)
+	r := mux.NewRouter()
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+	http.ListenAndServe(":8080", r)
+
+	fmt.Print("Hello")
 }
